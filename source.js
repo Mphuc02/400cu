f19adac719e9739521c10fcc04f7e1d1ba815a00
Mphuc02 <brolydinh@gmail.com>
2022-11-08 16:50:39 +0700

    400 cu khoai

---
+++ source.js
@@ -0,0 +1,5 @@
+var main_element = document.querySelector('.main-block')
+
+main_element.innerHTML =    `<div class="heart-block" style = "--i: ${1} ;">
+                                <img src="images.png" alt="">
+                            </div>`
